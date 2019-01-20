c622346ccaa0d142911c608b0501f7442ee2b98e
Dougllas Sousa <dougllasfps@gmail.com>
2019-01-20 16:52:50 -0300

    initial commit

---
+++ src/main/java/org/dougllas/securitycontrol/SecuritycontrolApplication.java
@@ -0,0 +1,14 @@
+package org.dougllas.securitycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritycontrolApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/dougllas/securitycontrol/SecuritycontrolApplicationTests.java
@@ -0,0 +1,17 @@
+package org.dougllas.securitycontrol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecuritycontrolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
