ebf17c092bd84a80dfd16bce7346d021a74a0c27
Dougllas Sousa <dougllasfps@gmail.com>
2019-07-18 11:07:55 -0300

    implementing module resource

--- src/main/java/org/dougllas/securitycontrol/api/resource/ModuleResource.java
+++ src/main/java/org/dougllas/securitycontrol/api/resource/ModuleResource.java
@@ -11,8 +11,11 @@ import org.dougllas.securitycontrol.service.ModuleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,8 +44,32 @@ public class ModuleResource {
 		return new ResponseEntity(HttpStatus.CREATED);
 	}
 	
+	@PutMapping("{id}")
+	public ResponseEntity update( @PathVariable("id") Long id, @Valid @RequestBody ModuleDTO module , BindingResult bindingResult) {
+		if(bindingResult.hasErrors()){
+            return new BadRequestResponseEntity(bindingResult);
+        }
+		
+		log.info("updating module {} " , id);
+		return service.findOne(id).map( m -> {
+			m.setName(module.getName());
+			service.update(m);
+			return ResponseEntity.ok(m);
+		}).orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND) );
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity delete( @PathVariable("id") Long id) {
+		log.info("deleting module {} " , id);
+		return service.findOne(id).map( m -> {
+			service.delete(m);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND) );
+	}
+	
 	@GetMapping
 	public List<Module> find( @RequestParam("name") String name ) {
+		log.info("searching module by name {} " , name);
 		return service.find(Module.builder().name(name).build());
 	}
 }
